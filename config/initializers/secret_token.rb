f4558a4c917008925925482c237f3e374b3676c3
Dave Copeland <davec@naildrivin5.com>
2014-03-08 14:19:58 -0500

    initial install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Receta::Application.config.secret_key_base = 'b563f0b364f7a8fcf2354a892e234ff45b8be0404ef67ae069efd19bc903b284fe1723c22f651fe6fe7f2bef7da8402c6ebe5c68ecb3cd462bf40b18e340db29'
