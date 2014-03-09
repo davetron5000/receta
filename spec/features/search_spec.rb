a9c75553713dac0376e28096653699fafc31f38a
Dave Copeland <davec@naildrivin5.com>
2014-03-09 14:27:51 -0400

    basic feature spec

---
+++ spec/features/search_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper.rb'
+
+feature "Looking up recipes", js: true do
+  scenario "finding recipes" do
+    visit '/'
+    fill_in "keywords", with: "baked"
+    click_on "Search"
+
+    expect(page).to have_content("Baked Potato")
+    expect(page).to have_content("Baked Brussel Sprouts")
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,40 @@
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+require 'rspec/autorun'
+require 'capybara/rails'
+
+Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
+
+ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+
+RSpec.configure do |config|
+  config.use_transactional_fixtures = true
+  
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with :truncation
+    DatabaseCleaner.clean_with :transaction
+  end
+
+  config.after(:each) do
+    ActionMailer::Base.deliveries.clear
+  end
+
+  config.around(:each, type: :feature, js: true) do |ex|
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.start
+    self.use_transactional_fixtures = false
+    ex.run
+    self.use_transactional_fixtures = true
+    DatabaseCleaner.clean
+  end
+
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.infer_base_class_for_anonymous_controllers = false
+  config.order = "random"
+end
