4d74a98bde5fb7d6d06f540740cb08ec02ecb2cf
Dave Copeland <davec@naildrivin5.com>
2014-03-15 20:51:19 -0400

    edit spec

---
+++ spec/features/edit_spec.rb
@@ -0,0 +1,37 @@
+require 'spec_helper.rb'
+
+feature "Creating, editing, and deleting a recipe", js: true do
+  scenario "CRUD a recipe" do
+    visit '/'
+    click_on "New Recipe…"
+
+    fill_in "name", with: "Baked Brussel Sprouts"
+    fill_in "instructions", with: "Slather in oil, then bake for 20 minutes"
+
+    click_on "Save"
+
+    expect(page).to have_content("Baked Brussel Sprouts")
+    expect(page).to have_content("Slather in oil")
+
+    click_on "Edit"
+
+    fill_in "name", with: "Roasted Brussel Sprouts"
+    fill_in "instructions", with: "Slather in oil, then roast for 20 minutes"
+
+    click_on "Save"
+
+    expect(page).to have_content("Roasted Brussel Sprouts")
+    expect(page).to have_content("then roast for 20 minutes")
+
+    visit "/"
+    fill_in "keywords", with: "Roasted"
+    click_on "Search"
+
+    click_on "Roasted Brussel Sprouts"
+
+    click_on "Delete"
+
+    expect(Recipe.find_by_name("Roasted Brussel Sprouts")).to be_nil
+
+  end
+end
