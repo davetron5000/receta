aa840863ceecda65b6aff2008966edb001f2092f
Dave Copeland <davec@naildrivin5.com>
2015-01-01 18:12:59 -0500

    update for Rails 4.2

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,5 @@
 //= require angular-route/angular-route
 //= require angular-resource/angular-resource
 //= require angular-flash/dist/angular-flash
+//= require angular-rails-templates
 //= require_tree .
