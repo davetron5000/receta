d8a7a41e206c544d33aa6e2962487deba8b02174
Jerrold Thompson <jet@whidbey.com>
2016-05-29 11:16:05 -0700

    initial commit after db:setup

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 20140309184135) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "recipes", force: true do |t|
+  create_table "recipes", force: :cascade do |t|
     t.string   "name"
     t.text     "instructions"
     t.datetime "created_at"
